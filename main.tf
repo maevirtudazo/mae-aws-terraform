de048f85842a262d839cbc928389e859382eda06
Mae Virtudazo <maevirtudazo6868@gmail.com>
2024-06-23 11:57:15 +0800

    added instances resources and security id

--- main.tf
+++ main.tf
@@ -17,3 +17,22 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default" {
   default = true
 }
+
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
+  tags = {
+    Name = "Learning Terraform"
+  }
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
