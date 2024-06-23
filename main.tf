f319fa8560f41935e277ead791c41eb197abb541
Mae Virtudazo <maevirtudazo6868@gmail.com>
2024-06-23 11:58:31 +0800

    added security config

--- main.tf
+++ main.tf
@@ -27,6 +27,23 @@ resource "aws_instance" "blog" {
     Name = "Learning Terraform"
   }
 }
+resource "aws_security_group" "blog" {
+  name = "blog"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
+
 
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
