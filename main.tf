66a9c8e3ea1459968840b32357b5fb31f1647332
Mae Virtudazo <maevirtudazo6868@gmail.com>
2024-06-30 11:53:56 +0800

    added more instances and added w/c key to use

--- main.tf
+++ main.tf
@@ -2,8 +2,42 @@ resource "aws_instance" "frontend-1" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+  key_name               = "mae"
 
   tags = {
-    Name = "Learning Terraform"
+    Name = "frontend-1"
+  }
+}
+
+resource "aws_instance" "frontend-2" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+  key_name               = "mae"
+
+  tags = {
+    Name = "frontend-2"
+  }
+}
+
+resource "aws_instance" "frontend-3" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+  key_name               = "mae"
+
+  tags = {
+    Name = "frontend-3"
+  }
+}
+
+resource "aws_instance" "load-balancer" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+  key_name               = "mae"
+
+  tags = {
+    Name = "load-balancer"
   }
 }
