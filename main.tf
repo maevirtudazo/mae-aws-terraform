63a208f82b21800ad7aef9e25ee202c91d20e96c
Mae Virtudazo <maevirtudazo6868@gmail.com>
2024-06-30 11:47:08 +0800

    separate security config to security.tf

--- main.tf
+++ main.tf
@@ -7,29 +7,3 @@ resource "aws_instance" "frontend-1" {
     Name = "Learning Terraform"
   }
 }
-resource "aws_security_group" "main" {
-  name = "blog"
-  tags = {
-    Terraform = "true"
-  }
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.main.id
-}
-
-
-resource "aws_security_group_rule" "https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.main.id
-}
---
+++ security.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "main" {
+  name = "blog"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
+
+
+resource "aws_security_group_rule" "https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
