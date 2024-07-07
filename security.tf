d6cd32e92ccfa29a05a43372512cd11703170e0d
Mae Virtudazo <maevirtudazo6868@gmail.com>
2024-07-07 11:26:31 +0800

    remove redundant vpc

--- security.tf
+++ security.tf
@@ -25,17 +25,17 @@ resource "aws_security_group_rule" "https_in" {
   security_group_id = aws_security_group.main.id
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
+#resource "aws_vpc" "my_vpc" {
+#  cidr_block = "10.0.0.0/16"
+#}
 
 resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.my_vpc.id
+  vpc_id = data.aws_vpc.default.id
   cidr_block = "10.0.2.0/24"
 }
 
 resource "aws_security_group" "lb_sg" {
-  vpc_id     = aws_vpc.my_vpc.id
+  vpc_id = data.aws_vpc.default.id
   ingress {
     from_port   = 80
     to_port     = 80
