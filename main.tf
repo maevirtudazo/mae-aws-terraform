f338a2a2015cb7ebb39d933a9a312b8c72ffa9ce
Mae Virtudazo <maevirtudazo6868@gmail.com>
2024-07-07 11:24:06 +0800

    added application load balancer

--- main.tf
+++ main.tf
@@ -34,14 +34,22 @@ resource "aws_instance" "frontend-3" {
   }
 }
 
-resource "aws_instance" "load-balancer" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.main.id]
-  key_name               = "mae"
-  instance_initiated_shutdown_behavior = "stop" 
+#resource "aws_instance" "load-balancer" {
+#  ami                    = data.aws_ami.app_ami.id
+#  instance_type          = var.instance_type
+#  vpc_security_group_ids = [aws_security_group.main.id]
+#  key_name               = "mae"
+#  instance_initiated_shutdown_behavior = "stop" 
 
-  tags = {
-    Name = "load-balancer"
-  }
+#  tags = {
+#    Name = "load-balancer"
+#  }
+#}
+
+resource "aws_lb" "my_lb" {
+  name               =  "my-app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.main.id]
+  subnets            = [aws_subnet.subnet1.id]
 }
--- security.tf
+++ security.tf
@@ -24,3 +24,22 @@ resource "aws_security_group_rule" "https_in" {
   cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
   security_group_id = aws_security_group.main.id
 }
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_security_group" "lb_sg" {
+  vpc_id     = aws_vpc.my_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
